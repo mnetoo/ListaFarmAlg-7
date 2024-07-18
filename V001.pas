1844505b1a0cd542361aa050ef45b8da63a932fe
mnetoo <163374075+mnetoo@users.noreply.github.com>
2024-07-17 21:22:26 -0300

    V001.pas

--- V001.pas
+++ V001.pas
@@ -1,47 +1,39 @@
 program V001;
 
-type vetor= array [1..200] of real;
+type
+    vetor = array [1..200] of real;
+
 var
-
     n, i: integer;
-    soma_posi, soma_neg: real;
     v: vetor;
-
-function par (x: integer): boolean;
-begin
-
-    if  (x mod 2 = 0) then  
-        par:= true
-    else
-        par:= false;
-
-end;
+    numerador, denominador: real;
+    
+    
 
 begin
 
-    read (n);
-    soma_posi:= 0;
-    soma_neg:= 0;
+    read(n);
+
+    if (n = 0) then begin
+        write('vetor vazio');
+        exit;
+    end;
 
         for i:= 1 to n do
-        begin
-
             read(v[i]);
-                if par (i) then begin
-                    if (v[i] > 0) then
-                        soma_posi:= soma_posi + v[i]
-                end
-                else
-                    if (v[i] < 0) then  
-                        soma_neg:= soma_neg + v[i];
-        
-        end;
 
-    if (n = 0) then
-        write('vetor vazio')
-    else if (soma_neg = 0) then
+        numerador:= 0;
+        denominador:= 0;
+
+            for i:= 1 to n do
+                if (i mod  2= 0) and (v[i]> 0) then
+                    numerador:= numerador + v[i]
+                else if (i mod 2 = 1) and (v[i] < 0) then
+                    denominador:= denominador + v[i];
+
+    if (denominador = 0) then
         write('divisao por zero')
     else
-        write((soma_posi/soma_neg):0:2);
+        write((numerador/denominador):0:2);
 
 end.
