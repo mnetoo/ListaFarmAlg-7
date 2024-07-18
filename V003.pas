2734459cb62b3fd815417c124f70d85d9ec94379
mnetoo <163374075+mnetoo@users.noreply.github.com>
2024-07-17 21:59:41 -0300

    V003.pas

--- V003.pas
+++ V003.pas
@@ -1,14 +1,14 @@
 program V003;
 
-type vetor = array [1..200] of real;
+type vetor = array [1..200] of integer;
+
 var
     v: vetor;
     n: integer;
 
-procedure ler_vetor (var w: vetor; var tam: integer);
+procedure lervetor (var w: vetor; var tam: integer);
 var
     i: integer;
-
 begin
     
     read(tam);
@@ -17,44 +17,44 @@ begin
 
 end;
 
-procedure imprime_vetor (var w: vetor; var tam: integer);
+function ordenado (var w: vetor; var tam: integer):boolean;
 var
     i: integer;
 begin
 
-    for i:= tam downto 1 do
-        write(w[i]:0:0, ' ');
-
-end;
-
-function ordenado (var w: vetor; var tam: integer): boolean;
-var
-    i: integer;
-begin
-    
     ordenado:= true;
 
-        for i:= 1 to (tam - 1) do 
-
-            if (w[i] > w[i + 1]) then 
-                ordenado:= false;
+        for i:= 1 to (tam-1) do
+            if (w[i] > w[i+1]) then
+                ordenado:= false;   
 
 end;
 
+procedure imprimevetor (var w: vetor; var tam: integer);
+var
+    i: integer;
 begin
 
-    ler_vetor(v, n);
-    
-    if ordenado (v, n) and (n <> 0) then
-        writeln('sim')
-    else begin
-        if (n = 0) then 
-            write('vetor vazio')
-        else
-            writeln('nao');
-    
+    for i:= tam downto 1 do 
+        write(w[i], ' ');
+
+end;
+
+
+begin
+
+    lervetor (v, n);
+
+    if (n = 0) then begin
+        write('vetor vazio');
+        exit;
     end;
 
-    imprime_vetor (v, n);
+    if ordenado (v, n) then
+        writeln('sim')
+    else
+        writeln('nao');
+
+    imprimevetor (v, n);
 
 end.
